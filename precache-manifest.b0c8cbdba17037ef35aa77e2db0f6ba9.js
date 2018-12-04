71cd0520748652e967aeb8bafaf8cf0080cd7439
asela <123azeela@gmail.com>
2018-12-05 01:54:25 +0530

    Updates

---
+++ precache-manifest.b0c8cbdba17037ef35aa77e2db0f6ba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "19bf91323f42d7d4a1c511c3482d6bc2",
+    "url": "/static/media/marker.19bf9132.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "658a79e7148717af6509",
+    "url": "/static/js/main.658a79e7.chunk.js"
+  },
+  {
+    "revision": "93724d0bae64dfda33a0",
+    "url": "/static/js/1.93724d0b.chunk.js"
+  },
+  {
+    "revision": "658a79e7148717af6509",
+    "url": "/static/css/main.d4766def.chunk.css"
+  },
+  {
+    "revision": "c3d5d1fd6321579512f2a852ee7960d2",
+    "url": "/index.html"
+  }
+];
