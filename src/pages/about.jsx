7f02ab6605419f1eae9b791f791b0cb90a535052
asela <123azeela@gmail.com>
2018-12-30 22:37:57 +0530

    about page mobile ready

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -108,10 +108,15 @@ export class AboutPage extends React.Component {
               </div>
             </div>
           </div>
+          <span className="heading">ACTIVITY</span>
+          <div className="row responsive">
 
-          <div class="arrow bounce" />
+          </div>
         </div>
-      </div>
+
+        <div class="arrow bounce" />
+
+      </div >
     );
   }
 }
