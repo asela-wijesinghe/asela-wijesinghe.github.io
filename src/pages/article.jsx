bec9dca3c15dca2667f77abc3122c5a483b6add3
Asela Wijesinghe <123azeela@gmail.com>
2020-05-11 02:48:47 +0530

    Basic article template set

--- src/pages/article.jsx
+++ src/pages/article.jsx
@@ -20,7 +20,7 @@ import {
   } from "react-share";
 import ReactMarkdown from "react-markdown";
 import MediaQuery from "react-responsive";
-
+import {Helmet} from "react-helmet";
 
 const articleDetails = [
   
@@ -33,44 +33,19 @@ const articleDetails = [
 ];
 
   const markdown = `
-  # Live demo <br/>
-  
-  Changes are automatically rendered as you type.
-  
-  ## Table of Contents
-  
-  * Implements [GitHub Flavored Markdown](https://github.github.com/gfm/)
-  * Renders actual, "native" React DOM elements
-  * Allows you to escape or skip HTML (try toggling the checkboxes above)
-  * If you escape or skip the HTML, no dangerouslySetInnerHTML is used! Yay!
-  
-  ## HTML block below
-  
-  <blockquote>
-    This blockquote will change based on the HTML settings above.
-  </blockquote>
-  
-  ## How about some code?
+  <h2>
+  What is Lorem Ipsum? </h2>
+<p>
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. <i>Lorem Ipsum has been the industry's standard</i> dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+</p>
+  <img src="https://i.imgur.com/F4VyJKC.jpg" width="100%">
+  <h3>
+  What is Lorem Ipsum? </h2>
+<p>
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. <i>Lorem Ipsum has been the industry's standard</i> dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+</p>
+  <img src="https://i.imgur.com/TRPTzTe.jpg" width="100%">
 
-  
-  Pretty neat, eh?
-  
-  ## Tables?
-  
-  | Feature   | Support |
-  | --------- | ------- |
-  | tables    | ✔ |
-  | alignment | ✔ |
-  | wewt      | ✔ |
-  
-  ## More info?
-  <b>
-  Read usage information and more on [GitHub](//github.com/rexxars/react-markdown)
-  </b>
-  
-  ---------------
-  
-  A component by [Espen Hovlandsdal](https://espen.codes/)
   `;
 export class ArticlePage extends React.Component {
     
@@ -101,16 +76,25 @@ export class ArticlePage extends React.Component {
     };
     return (
       <div className="container-article animated fadeIn">
-               
+        <Helmet>
+            <meta charSet="utf-8" />
+            <meta property="og:title" content={articleDetails[0].title} />
+            <meta property="og:description"
+                content={articleDetails[0].desc} />
+            <meta property="og:image" content="https://i.imgur.com/pbYcCyP.jpg" />
+            <meta property="og:url" content={"https://asela-wijesinghe.github.io/blog/"+articleDetails[0].linkID} />
+            <meta property="og:type" content="website" />
+            <title>{articleDetails[0].title}</title>
+            <link rel="canonical" href={"https://asela-wijesinghe.github.io/blog/"+articleDetails[0].linkID} />
+        </Helmet>
                <BackButton/>
                <MenuComponent/>
-               
-        {/* <img src={article} className="top-img-blog" /> */}
+                   {/* <img src={article} className="top-img-blog" /> */}
         <div className="card-article">
             <div className="card-title-container">
             <div className="card-title">
           <h1 className="heading">{articleDetails[0].title}</h1>
-          <h2>{articleDetails[0].desc}</h2>
+          <h3>{articleDetails[0].desc}</h3>
           <MediaQuery query="(max-width: 768px)">
         {renderShareLinks(articleDetails[0].linkID)}
         </MediaQuery>
